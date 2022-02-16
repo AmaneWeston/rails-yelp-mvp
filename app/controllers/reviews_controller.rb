f00b18ca8d25419542a4f155b3d6bfb0e6481c2a
Amane Weston <amane.weston02@gmail.com>
2022-02-16 16:50:31 +0900

    add restaurants and reviews controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,9 +7,9 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
-      @review.restaurant = @restaurant
-      recirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
